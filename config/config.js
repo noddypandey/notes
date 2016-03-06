5291e7cc50661c7f972308606c86345fa3743128
Nandan <nandan.pandey.noddy@gmail.com>
2016-03-06 20:32:44 +0530

    changed db user

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "port": process.env.PORT || "3000",
     "appName": "Notes",
-    "dbstring": "mongodb://nodecast:nodecast@ds023398.mlab.com:23398/nodecast"
+    "dbstring": "mongodb://noddy:noddy@ds023398.mlab.com:23398/nodecast"
 }
